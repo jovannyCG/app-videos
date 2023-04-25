087387673f69b695c5a9cafc992b44c98e034b57
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-25 14:42:40 -0600

    animacion de icono play terminada

--- lib/presentation/widgets/video_buttons.dart
+++ lib/presentation/widgets/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:app_videos/config/helpers/num_formats.dart';
 import 'package:app_videos/domain/entities/video_post.dart';
 import 'package:flutter/material.dart';
@@ -10,9 +11,26 @@ class VideoButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-       _IconButtonVideo(color:Colors.red, icon: Icons.favorite, value: video.likes,),
-       _IconButtonVideo( icon: Icons.remove_red_eye_outlined, value: video.likes,)
-        
+        _IconButtonVideo(
+          color: Colors.red,
+          icon: Icons.favorite,
+          value: video.likes,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        _IconButtonVideo(
+          icon: Icons.remove_red_eye_outlined,
+          value: video.likes,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SpinPerfect(
+            infinite: true,
+            duration: const Duration(seconds: 5),
+            child: const _IconButtonVideo(
+                icon: Icons.play_circle_fill_outlined, value: 0))
       ],
     );
   }
@@ -22,17 +40,21 @@ class _IconButtonVideo extends StatelessWidget {
   final int value;
   final IconData icon;
   final Color? color;
-  const _IconButtonVideo({required this.value, required this.icon, color}):color = color ??Colors.white;
+  const _IconButtonVideo({required this.value, required this.icon, color})
+      : color = color ?? Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-         IconButton(
-          color: color,
-          onPressed: (){}, 
-          icon: Icon(icon, size: 30,)),
-         Text(NumFormats.readbleNumber(value.toDouble()))
+        IconButton(
+            color: color,
+            onPressed: () {},
+            icon: Icon(
+              icon,
+              size: 30,
+            )),
+        if (value != 0) Text(NumFormats.readbleNumber(value.toDouble()))
       ],
     );
   }
