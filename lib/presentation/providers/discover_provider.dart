03535d51c97b626999205003e5c8fa8646bfe5a1
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-25 13:49:03 -0600

    cargade los videos al provider terminada

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,16 +1,24 @@
 
 
+import 'package:app_videos/config/infrastructure/models/local_video_model.dart';
 import 'package:app_videos/domain/entities/video_post.dart';
+import 'package:app_videos/shared/data/local_video_posts.dart';
 import 'package:flutter/material.dart';
 
 class DiscoverProvider extends ChangeNotifier{
   
   bool initialLoading = true;
-  List<VideoPost> video =[];
+  List<VideoPost> videos =[];
 
 
 Future<void> loadNextPage() async{
+await Future.delayed(const Duration(seconds: 2));
 
+final List<VideoPost> newVideos = videoPosts.map(
+  (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()).toList();
+
+  videos.addAll(newVideos);
+  initialLoading = false;
   notifyListeners();
 }
 }
